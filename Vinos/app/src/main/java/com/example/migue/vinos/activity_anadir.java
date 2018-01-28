a1be5bd02342a7f3bd0149f3bdf538b444af07f6
Miguel <micorreo@correo.com>
2018-01-28 23:13:36 +0100

    Versión 1.1
    Menú de inserción creado.

--- Vinos/app/src/main/java/com/example/migue/vinos/activity_anadir.java
+++ Vinos/app/src/main/java/com/example/migue/vinos/activity_anadir.java
@@ -4,10 +4,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
 public class activity_anadir extends AppCompatActivity implements View.OnClickListener {
 
     Button btnGrabar;
+    EditText nombre;
+    EditText denominacion;
+    CheckBox probado;
+    RadioButton tinto;
+    RadioButton blanco;
+    RatingBar rating;
+    String tipo = "";
+
+
+
     DBInterface dbInterface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +31,34 @@ public class activity_anadir extends AppCompatActivity implements View.OnClickLi
         btnGrabar = (Button) findViewById(R.id.btnGrabar);
         btnGrabar.setOnClickListener(this);
         dbInterface = new DBInterface(this);
+
+        nombre = (EditText)findViewById(R.id.edNombre);
+        denominacion = (EditText)findViewById(R.id.etDenominacion);
+        probado = (CheckBox) findViewById(R.id.cbProbado);
+        tinto = (RadioButton) findViewById(R.id.rbTinto);
+        blanco = (RadioButton) findViewById(R.id.rbBlanco);
+        rating = (RatingBar) findViewById(R.id.rbRating);
+
+        if (tinto.isChecked()){
+            tipo = "Tinto";
+        }else if(blanco.isChecked()){
+            tipo="Blanco";
+        }
+
+
+
         dbInterface.abre();
     }
     public void onClick(View v) {
-        dbInterface.insertarContacto("Vino1", "Denominacion1", true, "tipo1", "Descripcion1",1, "Imagen1");
+        int contador = 0;
+        try {
+            dbInterface.insertarContacto(nombre.getText().toString(), denominacion.getText().toString(), probado.isChecked(), tipo, "Descripcion", rating.getNumStars(), "Imagen");
+        }catch(Exception e){
+            Toast.makeText(getBaseContext(),"ERROR : en la inserción", Toast.LENGTH_LONG).show();
+            contador++;
+        }
+        if(contador==0){
+        Toast.makeText(getBaseContext(),"Se ha insertado correctamente", Toast.LENGTH_LONG).show();}
     }
     @Override
     protected void onStop(){
